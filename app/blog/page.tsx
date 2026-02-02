5191f9be4633142124038892b0011f582d9adfaa
chandra22455 <129649901+chandra22455@users.noreply.github.com>
2026-02-02 20:55:04 +0530

    Create page.tsx

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+  return (
+    <div className="max-w-3xl mx-auto p-8">
+      <h1 className="text-3xl mb-4">Blog</h1>
+      <p>Blog system coming soon 🚀</p>
+    </div>
+  );
+}
+
